a63e6c20c4ffce54eedcb9be9a58cb350b98f69d
ShaliniJha1999 <46892946+ShaliniJha1999@users.noreply.github.com>
2019-10-16 23:08:28 +0530

    Add files via upload

---
+++ interactive_traffic_light1.ino
@@ -0,0 +1,63 @@
+// Interactive Traffic Lights
+ 
+int carRed = 12; // To assign the car lights
+int carYellow = 11;
+int carGreen = 10;
+int pedRed = 9; // To assign the pedestrian lights
+int pedGreen = 8;
+int button = 2; // Tactile button pin
+int crossTime = 5000; // time allowed to cross
+ 
+unsigned long changeTime; // time since button pressed
+void setup() {
+pinMode(carRed, OUTPUT);
+pinMode(carYellow, OUTPUT);
+pinMode(carGreen, OUTPUT);
+pinMode(pedRed, OUTPUT);
+pinMode(pedGreen, OUTPUT);
+pinMode(button, INPUT); // button on pin 2
+// turn on the green light
+digitalWrite(carGreen, HIGH);
+digitalWrite(pedRed, HIGH);
+}
+void loop() {
+int state = digitalRead(button);
+/* check if button is pressed and it is
+over 5 seconds since last button press */
+if (state == HIGH && (millis() - changeTime) > 5000) {
+// Call the function to change the lights
+changeLights();
+Serial.print(state);
+}
+}
+void changeLights() {
+digitalWrite(carGreen, LOW); // green off
+digitalWrite(carYellow, HIGH); // yellow on
+delay(2000); // wait 2 seconds
+digitalWrite(carYellow, LOW); // yellow off
+digitalWrite(carRed, HIGH); // red on
+delay(1000); // wait 1 second till its safe
+digitalWrite(pedRed, LOW); // ped red off
+digitalWrite(pedGreen, HIGH); // ped green on
+delay(crossTime); // wait for preset time period
+// flash the ped green
+for (int x=0; x<10; x++) {
+digitalWrite(pedGreen, HIGH);    
+delay(250);
+digitalWrite(pedGreen, LOW);
+delay(250);
+}
+// turn ped red on
+digitalWrite(pedRed, HIGH);
+delay(500);
+digitalWrite(carYellow, HIGH); // Yellow will switch on
+digitalWrite(carRed, LOW); // red will switch off
+delay(1000);
+digitalWrite(carGreen, HIGH);
+digitalWrite(carYellow, LOW); // Yellow will switch off
+ 
+// record the time since last change of lights
+changeTime = millis();
+ 
+// Retun / Loop
+}
